0a5142533b2517dc1f235bef65e8bdf2814c0586
Trihandoyo41 <12050114323@students.uin-suska.ac.id>
2022-05-09 00:48:09 +0700

    Tugas Uts Alquran

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script xmlns="http://www.w3.org/1999/html"></script>
+
+<template class="bg">
+  <div class="background">
+    <div class="jumbotron">
+      <h1 class="display-4">Alquran Ku</h1>
+      <p class="lead">Website untuk membaca Al-Qur'an secara online dimanapun kamu berada</p>
+      <hr class="my-4">
+      <p>Website membaca dan mendengarkan Al-Qur'an </p>
+      <p>Ayo mulai dan membaca bersama kami</p>
+      <a class="btn btn-primary btn-lg" href="/random" role="button">Mulai</a>
+    </div>
+  </div>
+</template>
+<style>
+body {
+  background: url("../assets/silverbg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+</style>
